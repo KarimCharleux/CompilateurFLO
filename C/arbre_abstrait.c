1112edc26bf7c5ec4c522ef51b019821d570d219
Yvann PONCE <yvann.ponce@gmail.com>
2023-06-09 21:04:57 +0200

    reorder vars

--- C/arbre_abstrait.c
+++ C/arbre_abstrait.c
@@ -274,32 +274,18 @@ n_l_declaration* creer_n_l_declaration(n_variable* variable, n_l_declaration* l_
 {
 	n_l_declaration* n = malloc(sizeof(n_l_declaration));
 	n->variable = variable;
-	n->l_declaration = NULL;
+	n->l_declaration = l_declaration;
 
-	if(l_declaration==NULL)
-	{
-		return n;
-	}
-	n_l_declaration* l_declaration_pointer= l_declaration;
-	while (l_declaration_pointer->l_declaration != NULL)l_declaration_pointer = l_declaration_pointer->l_declaration;
-	l_declaration_pointer->l_declaration = n;
 
-	return l_declaration;
+
+	return n;
 }
 n_l_expression* creer_n_l_expression(n_exp* expression, n_l_expression* l_expression){
 	n_l_expression* n = malloc(sizeof(n_l_expression));
 	n->expression = expression;
-	n->l_expression = NULL;
+	n->l_expression = l_expression;
 
-	if(l_expression==NULL)
-	{
-		return n;
-	}
-	n_l_expression* l_expression_pointer= l_expression;
-	while (l_expression_pointer->l_expression != NULL)l_expression_pointer = l_expression_pointer->l_expression;
-	l_expression_pointer->l_expression = n;
-
-	return l_expression;
+	return n;
 }
 n_l_sinon_si* creer_n_l_sinon_si(n_condition* condition, n_l_sinon_si* l_sinon_si)
 {
